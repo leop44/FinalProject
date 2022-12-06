d4faacc84ad2772adb88db3e765860d27ad25c0e
Leo Padron <leopadron.4@gmail.com>
2022-12-05 20:26:03 -0500

    V.0.0.0
    Implementacion de controladores de movimiento

---
+++ Assets/Scripts/PlayerControllerRb.cs
@@ -0,0 +1,81 @@
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+
+public class PlayerControllerRb : MonoBehaviour
+{
+    private float hor;
+    public Rigidbody rb;
+    private Vector3 velocity;
+    private Vector3 startPos;
+    private float movSpeed = 10f;
+    private float jumpHeight = 300f;
+    private bool grounded;
+    private bool jump;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        Cursor.lockState = CursorLockMode.Locked; // Cursor Locked
+        Cursor.visible = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Movement & Rotation
+        hor = Input.GetAxisRaw("Horizontal");
+        velocity = Vector3.zero;
+
+        if (transform.position.y < -20) //Respawn by fall
+        {
+            Respawn();
+        }
+
+        CheckGround(); //Check ground for jump
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded) //Jump
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (grounded && !jump)
+        {
+            if (hor != 0)
+            {
+                Vector3 direction = (transform.right * hor).normalized;
+                velocity = direction * movSpeed;
+            }
+            velocity.y = rb.velocity.y;
+            rb.velocity = velocity;
+        }
+    }
+
+    private void Respawn() 
+    {
+        transform.position = startPos;
+    }
+
+    private void CheckGround() 
+    {
+        RaycastHit hitInfo;
+        Debug.DrawRay(transform.position, Vector3.down * 1.3f, Color.blue);
+        if (Physics.Raycast(transform.position, -Vector3.up, out hitInfo, 1.3f, LayerMask.GetMask("Ground")))
+        {
+            grounded = true;
+            jump = false;
+        }
+        else
+        {
+            grounded = false;
+            jump = true;
+        }
+    }
+
+    private void Jump() 
+    {
+        rb.AddForce(Vector3.up * jumpHeight);
+    }
+}
